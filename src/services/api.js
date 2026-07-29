680d32a313657b86bd62e59eb781c601908e4523
Afreeman <79459307+Andygurl5566@users.noreply.github.com>
2026-07-29 16:52:23 -0500

    Refactor handleCorrectJournal to include session retrieval and authentication check

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,27 @@
 import { supabase } from "../lib/supabase";
 
-
 // Send the text to the backend for analysis
 const {
   data: { session },
 } = await supabase.auth.getSession();
 
+// Send the text to the backend for analysis
 export async function handleCorrectJournal(
   text,
   nativeLanguage,
   targetLanguage,
 ) {
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session) {
+    throw new Error("User is not authenticated.");
+  }
+
+  console.log(session.access_token);
+  console.log("SESSION:", session);
+
   const response = await fetch("http://localhost:8000/journal/analyze", {
     method: "POST",
     headers: {
